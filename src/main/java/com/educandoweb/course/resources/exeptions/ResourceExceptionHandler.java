6226d8505dfc7288816eea0cc213649c834ff14f
Eduardo <eduardojunior010757@gmail.com>
2024-02-03 15:46:04 -0300

    try user delete

--- src/main/java/com/educandoweb/course/resources/exeptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/exeptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.educandoweb.course.services.exeptions.DatabaseException;
 import com.educandoweb.course.services.exeptions.ResourceNotFoundExeption;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +19,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandartError> resourceNotFound(ResourceNotFoundExeption errorClass, HttpServletRequest request){
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandartError errorResponse = new StandartError(Instant.now(), status.value(), error, errorClass.getMessage(), request.getServletPath());
+		return ResponseEntity.status(status).body(errorResponse);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandartError> databaseError(DatabaseException errorClass, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandartError errorResponse = new StandartError(Instant.now(), status.value(), error, errorClass.getMessage(), request.getServletPath());
 		return ResponseEntity.status(status).body(errorResponse);
 	}
