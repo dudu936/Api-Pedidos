12928f72f1ec6c58bf80831edb54ea1ad1a8edf8
Eduardo <eduardojunior010757@gmail.com>
2024-02-03 01:34:52 -0300

    exeptions findyById

---
+++ src/main/java/com/educandoweb/course/resources/exeptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.educandoweb.course.resources.exeptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.educandoweb.course.services.exeptions.ResourceNotFoundExeption;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundExeption.class)
+	public ResponseEntity<StandartError> resourceNotFound(ResourceNotFoundExeption errorClass, HttpServletRequest request){
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandartError errorResponse = new StandartError(Instant.now(), status.value(), error, errorClass.getMessage(), request.getServletPath());
+		return ResponseEntity.status(status).body(errorResponse);
+	}
+}
